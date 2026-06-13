8d3222e428ee2305ab83525106cb6a4e403f0882
ZH_Tailscale <bioinfo.gao@gmail.com>
2026-06-13 12:00:00 -0400

    e

---
+++ 1_opossum_YuFan/script_opposum/NA_check_liftoff_output.sh
@@ -0,0 +1,9 @@
+cd /Work_bio/references/Didelphis_virginiana/mDidVir1/DNA_Zoo/
+
+ls -lh Didelphis_v.liftoff.gtf unmapped.txt
+ls -lh 
+# Didelphis_v.liftoff.gtf：这是你最重要的结果文件（北美负鼠的注释）。
+# unmapped.txt：记录了那些无法从南美负鼠迁移到北美负鼠的基因 ID。
+# Didelphis_v.liftoff.gff3
+
+head Didelphis_v.liftoff.gtf
