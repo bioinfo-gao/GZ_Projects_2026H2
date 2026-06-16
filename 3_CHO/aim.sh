68d1d29632accd18b8ce7104858a77976c2c0639
ZH_Tailscale <bioinfo.gao@gmail.com>
2026-06-16 18:02:53 -0400

    i

---
+++ 3_CHO/aim.sh
@@ -0,0 +1,7 @@
+# /home/gao/Dropbox/Keqiang
+
+# CHO genome wgs data
+# Confirmation of CHO cell lines from Chinese hamster ovaries
+
+# ls -lh /home/gao/Dropbox/Keqiang/*R1* /home/gao/Dropbox/Keqiang/*r1* /home/gao/Dropbox/Keqiang/*.fq.gz /home/gao/Dropbox/Keqiang/*.fastq.gz 2>/dev/null | head -30
+ls -lh /home/gao/Dropbox/Keqiang/
