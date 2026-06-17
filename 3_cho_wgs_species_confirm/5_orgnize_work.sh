4e1af9eefd216b2b86b6a7c2347c51057b1b2773
ZH_Tailscale <bioinfo.gao@gmail.com>
2026-06-17 12:28:14 -0400

    x

--- 3_cho_wgs_species_confirm/5_orgnize_work.sh
+++ 3_cho_wgs_species_confirm/5_orgnize_work.sh
@@ -5,7 +5,10 @@ cp /home/gao/projects_2026H2/3_cho_wgs_species_confirm/qc/wt1_fastp.html /home/g
 
 cp /home/gao/projects_2026H2/3_cho_wgs_species_confirm/qc/multiqc/multiqc_report.html /home/gao/projects_2026H2/3_cho_wgs_species_confirm/delivery/qc/
 
+
 cp /home/gao/projects_2026H2/3_cho_wgs_species_confirm/results/cho_flagstat.txt /home/gao/projects_2026H2/3_cho_wgs_species_confirm/delivery/results/
+
+cp /home/gao/projects_2026H2/3_cho_wgs_species_confirm/results/dhfr_gff.txt /home/gao/projects_2026H2/3_cho_wgs_species_confirm/delivery/results/
 
 tree /home/gao/projects_2026H2/3_cho_wgs_species_confirm/delivery/
 
