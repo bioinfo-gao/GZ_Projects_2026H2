c4f2a90b28aac419f7aad22c70ea65c6d8cc379e
ZH_Tailscale <bioinfo.gao@gmail.com>
2026-06-17 22:56:19 -0400

    p

--- 1_opossum_YuFan/script_opposum/3A_further_progress_check.sh
+++ 1_opossum_YuFan/script_opposum/3A_further_progress_check.sh
@@ -32,7 +32,3 @@ tail -3 /home/gao/projects_2026H2/1_opossum_YuFan/script_opposum/work/8c/bfffa03
 # [-        ] NFCORE_RNASEQ:RNASEQ:ALIGN_STAR:BAM_SORT_STATS_SAMTOOLS:BAM_STATS_SAMTOOLS:SAMTOOLS_IDXSTATS -
 # [-        ] NFCORE_RNASEQ:RNASEQ:QUANTIFY_STAR_SALMON:SALMON_QUANT                                       [  0%] 0 of 5
 
-tail -3 /home/gao/projects_2026H2/1_opossum_YuFan/script_opposum/work/8c/bfffa0314b7b899be50ccf24bad072/NC_4.Log.progress.out
-Jun 17 22:48:39      5.9    64079567      296    87.8%    290.6     0.7%     3.2%     0.0%     0.0%     9.0%     0.0%
-Jun 17 22:49:41      5.9    64205446      296    87.8%    290.6     0.7%     3.2%     0.0%     0.0%     9.0%     0.0%
-Jun 17 22:51:14      5.8    64289524      296    87.8%    290.6     0.7%     3.2%     0.0%     0.0%     9.0%     0.0%
