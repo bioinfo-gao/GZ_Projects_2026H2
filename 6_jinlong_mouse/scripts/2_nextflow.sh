a11d86f357c5e77450c8befd0cee0bca81e1b806
ZH_Tailscale <bioinfo.gao@gmail.com>
2026-06-26 21:31:59 -0400

    p

--- 6_jinlong_mouse/scripts/2_nextflow.sh
+++ 6_jinlong_mouse/scripts/2_nextflow.sh
@@ -11,26 +11,27 @@ tmux kill-session -t rnaseq 2>/dev/null || true
 export NXF_OPTS="-Xms512m -Xmx2g"
 export NXF_SINGULARITY_CACHEDIR="/home/gao/.singularity/nf-core"
 
-tmux new-session -d -s rnaseq "
-  export NXF_OPTS='-Xms512m -Xmx2g'
-  export NXF_SINGULARITY_CACHEDIR='/home/gao/.singularity/nf-core'
-  cd /home/gao/projects_2026H2/6_jinlong_mouse/scripts
+tmux new-session -d -s rnaseq 
+export NXF_OPTS='-Xms512m -Xmx2g'
+export NXF_SINGULARITY_CACHEDIR='/home/gao/.singularity/nf-core'
+cd /home/gao/projects_2026H2/6_jinlong_mouse/scripts
 
-  nextflow run nf-core/rnaseq \
-      -r 3.15.1 \
-      -profile singularity \
-      -c local_optimized.config \
-      --input nf_core_samplesheet.csv \
-      --outdir ../output_results \
-      --fasta /Work_bio/references/Mus_musculus/GRCm39/mouse_gencode_M35/GRCm39.primary_assembly.genome.fa \
-      --gtf /Work_bio/references/Mus_musculus/GRCm39/mouse_gencode_M35/gencode.vM35.annotation.gtf \
-      --star_index '/Work_bio/references/Mus_musculus/GRCm39/mouse_gencode_M35/star_index' \
-      --gencode \
-      --aligner star_salmon \
-      --max_cpus 28 \
-      --max_memory '108.GB' \
-      2>&1 | tee nextflow_run.log
-"
+nextflow run nf-core/rnaseq \
+    -r 3.15.1 \
+    -profile singularity \
+    # -resume
+    -c local_optimized.config \
+    --input nf_core_samplesheet.csv \
+    --outdir ../output_results \
+    --fasta /Work_bio/references/Mus_musculus/GRCm39/mouse_gencode_M35/GRCm39.primary_assembly.genome.fa \
+    --gtf /Work_bio/references/Mus_musculus/GRCm39/mouse_gencode_M35/gencode.vM35.annotation.gtf \
+    --star_index '/Work_bio/references/Mus_musculus/GRCm39/mouse_gencode_M35/star_index' \
+    --gencode \
+    --aligner star_salmon \
+    --max_cpus 28 \
+    --max_memory '108.GB' \
+    2>&1 | tee nextflow_run.log
+
 
 echo "Nextflow started in tmux session 'rnaseq'"
 echo "Monitor: tmux attach -t rnaseq"
